221e9f668ecd8ee8fe536276b4ae020fb67f0b13
Ryan Binhote <ryanbm22@gmail.com>
2025-04-25 14:54:19 -0300

    `Adicionado um loop while para solicitar repetidamente a entrada do usuário até que o número correto seja adivinhado.`

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,15 +1,19 @@
 alert('Boas Vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute 
 
-// se o chute for igual ao núemro secreto
-if (chute == numeroSecreto ) {
-    alert(`Isso ai! Você decobriu o ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+//enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    // se o chute for igual ao núemro secreto
+    if (chute == numeroSecreto ) {
+        alert(`Isso ai! Você decobriu o ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
