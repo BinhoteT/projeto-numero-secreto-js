b8bdeb38bfcd74b98263f0d14d4a881f210c05d5
Ryan Binhote <ryanbm22@gmail.com>
2025-04-25 13:42:49 -0300

    Atualizando dados

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert('Boas Vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 9;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
 
+// se o chute for igual ao núemro secreto
 if (chute == numeroSecreto ) {
-    console.log('Isso ai! Você decobriu o número secreto (5)');
+    alert(`Isso ai! Você decobriu o ${numeroSecreto}`);
+} else {
+    alert('Você errou :(')
 }
