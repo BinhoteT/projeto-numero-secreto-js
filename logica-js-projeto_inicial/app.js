17ba40c9e1fc90a70110851858f9ca0bebf10f52
Ryan Binhote <ryanbm22@gmail.com>
2025-04-28 10:39:50 -0300

    Adicionado prompt para entrada do usuário e registro do número secreto, mensagem de vitória atualizada para incluir o número de tentativas.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,19 +1,22 @@
 alert('Boas Vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute 
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 30');
     // se o chute for igual ao núemro secreto
     if (chute == numeroSecreto ) {
-        alert(`Isso ai! Você decobriu o ${numeroSecreto}`);
+        alert(`Isso ai! Você decobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+       // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
