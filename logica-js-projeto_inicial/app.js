d3bfe36be6de17d20b58e8a4213d4da47b813077
Ryan Binhote <ryanbm22@gmail.com>
2025-04-28 12:17:20 -0300

    Refatore app.js para gerar um número secreto aleatório entre 1 e um valor máximo definido pelo usuário.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,13 @@
 alert('Boas Vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 30');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se o chute for igual ao núemro secreto
     if (chute == numeroSecreto ) {
         break;
